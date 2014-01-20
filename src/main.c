0f18cd1a3b8854778f70ec246719e0ca74f01e68
Iván Stepaniuk <istepaniuk@gmail.com>
2014-01-20 22:20:07 +0100

    printing data from the 3 sensors to the USART

--- src/main.c
+++ src/main.c
@@ -7,10 +7,14 @@
 
 void delay(int count) { while(count--){}; }
 
+void print_sensor_data(struct am2302_sensor_data_t sdata)
+{
+    usart_puts(" humi: "); printint(sdata.humidity); usart_puts("\n");
+    usart_puts(" temp: "); printint(sdata.temperature); usart_puts("\n");
+}
+
 int main(void)
 {
-    struct am2302_sensor_data_t sdata;
-        
     // Enable GPIO clock
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
@@ -23,16 +27,16 @@ int main(void)
     am2302_init();
     
     leds_turn_blue_on();
-
+    
+    int i = 0;
     while(1) { 
         leds_turn_green_on();
-        am2302_start_acquire(1);
+        am2302_start_acquire(i % 3);
         while(!am2302_acquire_has_finished()) { }
-
-        sdata = am2302_get_sensor_data();
+        usart_puts("\nsensor: "); printint(i%3); usart_puts("\n");
+        print_sensor_data(am2302_get_sensor_data());
         leds_turn_green_off();
-        usart_puts("humi: "); printint(sdata.humidity); usart_puts("\n");
-        usart_puts("temp: "); printint(sdata.temperature); usart_puts("\n");
-        delay(9000000);
+        delay(3000000);
+        i++;
     }
 }
